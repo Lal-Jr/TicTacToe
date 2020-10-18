9e90250cec4221044309fcc71d84ed9c07698b4a
Harish Lal <harishlal80@gmail.com>
2020-10-18 12:08:21 +0530

    JS Added

--- Game.js
+++ Game.js
@@ -1,1 +1,10 @@
-console.log("Connected");
+// Restart the Game
+var restart = document.querySelector("#butt");
+// All Squares
+var squares = document.querySelectorAll("td");
+// Clear all Squares
+function clearBoard(){
+  for(var i = 0; i < squares.length; i++){
+    squares[i].textContent = '';
+  }
+}
